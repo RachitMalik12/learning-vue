feeb78ac2fa14e11e5051cfc23fd7c1045a4e7ea
Rachit Malik <rachitmalik1947@gmail.com>
2020-03-27 16:24:20 +0530

    Added mock api requests

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
  import Todos from './components/Todos'
  import Header from './components/layouts/Header'
  import AddTodo from './components/AddTodo'
+ import axios from 'axios'
 
 export default {
   name: 'App',
@@ -20,32 +21,31 @@ export default {
   },
   data(){
     return {
-      todos: [
-        {
-          id: 1, 
-          title: "Todo one", 
-          completed: false
-        }, 
-        {
-          id: 2, 
-          title: "Fake todo", 
-          completed: false
-        },
-        {
-          id:3,
-          title: "Learn Vue!", 
-          completed: false 
-        }
-      ]
+      todos: []
     }
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id); 
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(this.todos = this.todos.filter(todo => todo.id !== id))
+      .catch(err => console.log(err))
+
+      
     },
     addTodo(todo) {
-      this.todos = [... this.todos, todo]
+      const {title, completed} = todo
+      axios.post("https://jsonplaceholder.typicode.com/todos", {
+        title, 
+        completed
+      })
+      .then (res => this.todos = [...this.todos, res.data])
+      .catch(err => console.log(err))
     }
+  },
+  created() {
+    axios.get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+    .then(res => this.todos = res.data)
+    .catch(err => console.log(err))
   }
 }
 </script>
