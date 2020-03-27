d2fbf49eba13af398d1b02c1eac4673760eff09e
Rachit Malik <rachitmalik1947@gmail.com>
2020-03-27 13:24:06 +0530

    Added form to add new todo and header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
+    <Header/>
+    <AddTodo/>
     <Todos v-bind:todos = "todos" v-on:del-todo = "deleteTodo"/>
   </div>
 </template>
 
 <script>
  import Todos from './components/Todos'
+ import Header from './components/layouts/Header'
+ import AddTodo from './components/AddTodo'
 
 export default {
   name: 'App',
   components: {
-    Todos 
+    Todos,
+    Header,
+    AddTodo
   },
   data(){
     return {
@@ -52,4 +58,15 @@ body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
 }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
 </style>
